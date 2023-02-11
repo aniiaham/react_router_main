894a29a8a1655e7d7429ee859c2ae412adb20907
aniiaham <aniya_axm@icloud.com>
2023-02-10 22:29:35 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
-  { path: '/events', element: <EventsPage />}
+  { path: '/events', element: <EventsPage /> },
+  { path: '/events/:eventsId', element: <EventDetailPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/id/edit', element: <EditEventPage />}
 ]);
 
 function App() {
