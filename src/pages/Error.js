eabc46b149dee3517bd5f3f6b8443b1f944488e1
aniiaham <aniya_axm@icloud.com>
2023-02-15 17:01:32 -0500

    error.js complited

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,30 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
 import PageContent from "../components/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.staus === 404) {
+    title = "Not found!";
+    message = "Could not find resourse or page.";
+  }
+
   return (
-    <PageContent title="An error occured!">
-      <p>Something went wrong!</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 }
 
