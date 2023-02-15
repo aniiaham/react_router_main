423aa799f3417f2f2e3a4af3373309032f33c350
aniiaham <aniya_axm@icloud.com>
2023-02-15 17:07:24 -0500

    json function

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,6 +1,6 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
@@ -12,7 +12,7 @@ function EventsPage() {
   const events = data.events;
   return (
     <>
-      <EventsList events={events}/>
+      <EventsList events={events} />
     </>
   );
 }
@@ -20,13 +20,17 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
-  console.log(response)
+  const response = await fetch("http://localhost:8080/events");
+  console.log(response);
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.'};
-    throw new Response(JSON.stringify({message: 'Could not fetch events'}), {status: 500});
+    //   throw new Response(JSON.stringify({message: 'Could not fetch events'}), {status: 500});
+    // } else {
+    //   return response;
+    // }
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
-};
+}
